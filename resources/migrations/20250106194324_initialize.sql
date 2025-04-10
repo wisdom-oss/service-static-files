6589fa83b3c6ba5a7e6d794a2982787d72718b5a
Jan Eike Suchard <jan@suchard.cloud>
2025-04-10 11:34:32 +0200

    chore(db): adding service registration

--- resources/migrations/20250106194324_initialize.sql
+++ resources/migrations/20250106194324_initialize.sql
@@ -1,7 +1,19 @@
--- TODO: Write required database migrations here
-
-
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'no migrations to run';
+CREATE SCHEMA IF NOT EXISTS auth;
+
+CREATE TYPE auth.scope_level AS ENUM('read', 'write', 'delete', '*');
+
+CREATE TABLE IF NOT EXISTS
+    auth.services (
+        id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid (),
+        "name" TEXT NOT NULL UNIQUE,
+        description TEXT,
+        supported_scope_levels auth.scope_level[]
+    );
+
+INSERT INTO
+    auth.services ("name", supported_scope_levels)
+VALUES
+    ('static-files', '{read, write, delete, *}');
 -- +goose StatementEnd
