6db989a6669c7eb7d20e5c2af61aa8020ec16ba6
Jan Eike Suchard <jan@suchard.cloud>
2025-04-10 14:36:44 +0200

    chore: implementing route for uploading object

--- routes/v1/upload.go
+++ routes/v1/upload.go
@@ -2,18 +2,22 @@ package v1
 
 import (
 	"io"
+	"log/slog"
 	"net/http"
+	"path"
+	"sync"
 
 	"github.com/gabriel-vasile/mimetype"
 	"github.com/gin-gonic/gin"
 	"github.com/minio/minio-go/v7"
+	"golang.org/x/sync/errgroup"
 
 	objectStorage "microservice/internal/minio"
 )
 
 func Upload(c *gin.Context) {
 	bucket := c.Param("bucket")
-	filename := c.Param("filename")
+	basepath := c.Param("basepath")
 
 	client := objectStorage.Client()
 	bucketExists, err := client.BucketExists(c, bucket)
@@ -32,43 +36,66 @@ func Upload(c *gin.Context) {
 		}
 	}
 
-	uploadFile, err := c.FormFile("file")
+	form, err := c.MultipartForm()
 	if err != nil {
 		c.Abort()
 		_ = c.Error(err)
 		return
 	}
 
-	f, err := uploadFile.Open()
-	if err != nil {
+	files, ok := form.File["file"]
+	if !ok {
+		// todo: send error
+		return
+	}
+
+	var uploadGroup errgroup.Group
+	var uploadResults []minio.UploadInfo
+	var mutex sync.Mutex
+
+	for _, file := range files {
+
+		uploadGroup.Go(func() error {
+			slog.Warn("uploading file", "filename", file.Filename)
+			f, err := file.Open()
+			if err != nil {
+				return err
+			}
+
+			mime, err := mimetype.DetectReader(f)
+			if err != nil {
+				return err
+			}
+
+			_, err = f.Seek(0, io.SeekStart)
+			if err != nil {
+				return err
+			}
+
+			filename := path.Join(basepath, file.Filename)
+
+			info, err := client.PutObject(c, bucket, filename, f, file.Size, minio.PutObjectOptions{
+				ContentType: mime.String(),
+			})
+
+			if err != nil {
+				return err
+			}
+
+			mutex.Lock()
+			uploadResults = append(uploadResults, info)
+			mutex.Unlock()
+			return nil
+
+		})
+	}
+
+	if err := uploadGroup.Wait(); err != nil {
 		c.Abort()
 		_ = c.Error(err)
 		return
 	}
 
-	mime, err := mimetype.DetectReader(f)
-	if err != nil {
-		c.Abort()
-		_ = c.Error(err)
-		return
-	}
-
-	_, err = f.Seek(0, io.SeekStart)
-	if err != nil {
-		c.Abort()
-		_ = c.Error(err)
-		return
-	}
-
-	info, err := client.PutObject(c, bucket, filename, f, uploadFile.Size, minio.PutObjectOptions{
-		ContentType: mime.String(),
-	})
-	if err != nil {
-		c.Abort()
-		_ = c.Error(err)
-		return
-	}
-
-	c.Header("ETag", info.ETag)
 	c.Status(http.StatusCreated)
+
 }
