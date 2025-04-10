884877f8e6b762faa10919cf5a6fb575eaa416a4
Jan Eike Suchard <jan@suchard.cloud>
2025-04-10 14:35:38 +0200

    chore: adding constant for unknown bucket

--- routes/v1/consts.go
+++ routes/v1/consts.go
@@ -1,8 +1,6 @@
 package v1
 
-import "microservice/internal"
-
 // This file contains constants that are used throughout the routes.
 
 const ErrNoSuchKey = "NoSuchKey"
-const defaultBucket = internal.RootBucketName
+const ErrNoSuchBucket = "NoSuchBucket"
